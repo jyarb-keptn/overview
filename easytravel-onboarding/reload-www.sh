ba844538dc5c528c865c46477e1b67034112d8f1
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-13 13:46:39 -0400

    Create reload-www.sh

---
+++ easytravel-onboarding/reload-www.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+git pull
+
+keptn delete service easytravel-www --project=easytravel
+
+keptn onboard service easytravel-www --project=easytravel --chart=./easytravel-www
+
+keptn add-resource --project=easytravel --service=easytravel-www --stage=staging --resource=jmeter/www/www-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+
+keptn add-resource --project=easytravel --service=easytravel-www --stage=production --resource=jmeter/www/www-basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
+
+keptn add-resource --project=easytravel --service=easytravel-www --stage=staging --resource=jmeter/www/jmeter.conf.yaml --resourceUri=jmeter/jmeter.conf.yaml
+
+keptn add-resource --project=easytravel --service=easytravel-www --stage=production --resource=jmeter/www/jmeter.conf.yaml --resourceUri=jmeter/jmeter.conf.yaml
+
+keptn add-resource --project=easytravel --stage=staging --service=easytravel-www --resource=simple_slo.yaml --resourceUri=slo.yaml
+
+keptn add-resource --project=easytravel --stage=production --service=easytravel-www --resource=simple_slo.yaml --resourceUri=slo.yaml
+
+keptn trigger delivery --project=easytravel --service=easytravel-www --image=docker.io/dynatrace/easytravel-nginx --tag=latest --labels=creator=cli
