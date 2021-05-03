da16d48c4495ac3e33790e3f55b974b9ebd0a4b1
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-05-03 13:38:03 -0400

    Update reset-orders.sh

--- keptn-onboarding/reset-orders.sh
+++ keptn-onboarding/reset-orders.sh
@@ -2,6 +2,7 @@
 
 KEPTN_IN_A_BOX_DIR="~/keptn-in-a-box"
 KEPTN_CATALOG_DIR="~/overview"
+DOMAIN="192.168.3.91.nip.io"
 
 keptn delete project keptnorders
 
@@ -10,12 +11,12 @@ kubectl delete namespace keptnorders-staging
 
 git pull
 
-cd $KEPTN_CATALOG_DIR/keptn-onboarding/ 
-$KEPTN_IN_A_BOX_DIR/resources/catalog/onboard_catalog.sh
-$KEPTN_IN_A_BOX_DIR/resources/catalog/onboard_catalog_qualitygates.sh
+cd ~/overview/keptn-onboarding/ 
+~/keptn-in-a-box/resources/catalog/onboard_catalog.sh
+~/keptn-in-a-box/resources/catalog/onboard_catalog_qualitygates.sh
 
-$KEPTN_IN_A_BOX_DIR/resources/catalog/deploy_catalog_0.1.sh
-$KEPTN_IN_A_BOX_DIR/resources/catalog/deploy_catalog_0.2.sh
+~/keptn-in-a-box/resources/catalog/deploy_catalog_0.1.sh
+~/keptn-in-a-box/resources/catalog/deploy_catalog_0.2.sh
 
-cd $KEPTN_IN_A_BOX_DIR/resources/ingress 
+cd ~/keptn-in-a-box/resources/ingress 
 ./create-ingress.sh ${DOMAIN} keptnorders
