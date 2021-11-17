997e2c90ffe5fd6a898a4dfb7589ba5be9e657c4
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-11-17 09:38:48 -0500

    Update loadRemediation.sh

--- keptn-onboarding/loadRemediation.sh
+++ keptn-onboarding/loadRemediation.sh
@@ -4,3 +4,8 @@ keptn add-resource --project=keptnorders --stage=production --service=customer -
 keptn add-resource --project=keptnorders --stage=production --service=catalog --resource=remediation.yaml --resourceUri=remediation.yaml
 keptn add-resource --project=keptnorders --stage=production --service=order --resource=remediation.yaml --resourceUri=remediation.yaml
 keptn add-resource --project=keptnorders --stage=production --service=frontend --resource=remediation.yaml --resourceUri=remediation.yaml
+
+keptn add-resource --project=keptnorders --stage=staging --service=customer --resource=remediation.yaml --resourceUri=remediation.yaml
+keptn add-resource --project=keptnorders --stage=staging --service=catalog --resource=remediation.yaml --resourceUri=remediation.yaml
+keptn add-resource --project=keptnorders --stage=staging --service=order --resource=remediation.yaml --resourceUri=remediation.yaml
+keptn add-resource --project=keptnorders --stage=staging --service=frontend --resource=remediation.yaml --resourceUri=remediation.yaml
