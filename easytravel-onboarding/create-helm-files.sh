16f595e463734a7233d881f860167b068b3e8c7b
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-01-20 11:45:03 -0500

    create tars

--- easytravel-onboarding/create-helm-files.sh
+++ easytravel-onboarding/create-helm-files.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-rm 
+echo "remove current tgz files...."
+rm easytravel-angular.tgz
+rm easytravel-www.tgz
+rm easytravel-backend.tgz
+rm easytravel-frontend.tgz
+rm easytravel-mongodb.tgz
+
+echo "Creating tgz files...."
+
+tar -czvf easytravel-angular.tgz /easytravel-angular
+tar -czvf easytravel-www.tgz /easytravel-www
+tar -czvf easytravel-backend.tgz /easytravel-backend
+tar -czvf easytravel-frontend.tgz /easytravel-frontend
+tar -czvf easytravel-mongodb.tgz /easytravel-mongodb
+
+echo "tarballs created...."
